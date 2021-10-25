392152a2d29a71084b449ab099126dd56b6afd6d
Geeta Ingle <42793643+geetaingle@users.noreply.github.com>
2021-10-25 13:31:27 +0530

    Updates

---
+++ precache-manifest.00de21d8124e5f234e52537d17b3471f.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5933b67ef7e531e784b041e35f576341",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e41295d055d24ed4a56d",
+    "url": "/portfolio/static/css/main.5d94d4c0.chunk.css"
+  },
+  {
+    "revision": "7e192b145cf0b6ec953f",
+    "url": "/portfolio/static/js/2.2a8d2103.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.2a8d2103.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e41295d055d24ed4a56d",
+    "url": "/portfolio/static/js/main.f5e13089.chunk.js"
+  },
+  {
+    "revision": "5297f37e06cf542621cb",
+    "url": "/portfolio/static/js/runtime-main.664b97c1.js"
+  },
+  {
+    "revision": "43db1d78cec98e8d4dd7a24e9a3a8a6e",
+    "url": "/portfolio/static/media/ResumeV3.43db1d78.pdf"
+  },
+  {
+    "revision": "d8b1766edd9468e247627ae7758a3ea6",
+    "url": "/portfolio/static/media/SAV.d8b1766e.png"
+  },
+  {
+    "revision": "05b389be17670a3208154f6536cfe9ac",
+    "url": "/portfolio/static/media/appleBadge.05b389be.webp"
+  },
+  {
+    "revision": "3a7465cc83ca069445217f9aa86b24f3",
+    "url": "/portfolio/static/media/backEnd.3a7465cc.webp"
+  },
+  {
+    "revision": "92ddcdceeccc734dc84d4ba2ca0cce63",
+    "url": "/portfolio/static/media/chatdemo.92ddcdce.gif"
+  },
+  {
+    "revision": "345e135152cc1d40c0e04842b2ecc633",
+    "url": "/portfolio/static/media/chatdemovideo.345e1351.mp4"
+  },
+  {
+    "revision": "8e5a5528c75b1ca972b371ab0ec816a5",
+    "url": "/portfolio/static/media/exploreSpace.8e5a5528.png"
+  },
+  {
+    "revision": "b53ffe85ca7a2ef537770128b1ab701b",
+    "url": "/portfolio/static/media/frontEnd.b53ffe85.webp"
+  },
+  {
+    "revision": "b03fda8ea001d0d56574b5b82ea8a0b1",
+    "url": "/portfolio/static/media/musicalchat.b03fda8e.webp"
+  },
+  {
+    "revision": "f0f7cf07752714e442482809e85dedc6",
+    "url": "/portfolio/static/media/pinterestcloneIMG.f0f7cf07.png"
+  },
+  {
+    "revision": "02e9ebf6b600c474fb2192673c381552",
+    "url": "/portfolio/static/media/polyblock.02e9ebf6.webp"
+  },
+  {
+    "revision": "7ddb225896737aaea2b00530b0819931",
+    "url": "/portfolio/static/media/polyblock1.7ddb2258.webp"
+  },
+  {
+    "revision": "5a6963b61211d03d7a53e20d9a2b75d3",
+    "url": "/portfolio/static/media/polyblock2.5a6963b6.webp"
+  },
+  {
+    "revision": "e6214287417c510ac34a77e320e52fc5",
+    "url": "/portfolio/static/media/polyblock3.e6214287.webp"
+  },
+  {
+    "revision": "4b76a530e6d8af6de8ec301ca8251f28",
+    "url": "/portfolio/static/media/portfolio.4b76a530.png"
+  },
+  {
+    "revision": "793312a96f1d89a43f7a6d12ed4a7fca",
+    "url": "/portfolio/static/media/ratemyart.793312a9.webp"
+  },
+  {
+    "revision": "8f8619192878b3e9f7dd813f62d272b4",
+    "url": "/portfolio/static/media/ratemyart1.8f861919.webp"
+  },
+  {
+    "revision": "1696e10757ee7a5bcd9ad3e276a8ec82",
+    "url": "/portfolio/static/media/ratemyart2.1696e107.webp"
+  },
+  {
+    "revision": "0d7d81fdc1635c5a45ca9094d77144ca",
+    "url": "/portfolio/static/media/ratemyart3.0d7d81fd.webp"
+  },
+  {
+    "revision": "36d87bcf17ebc0ef3b3a1b4a91fbc70c",
+    "url": "/portfolio/static/media/sortingAlgo.36d87bcf.gif"
+  },
+  {
+    "revision": "7ac0812bf5578b8689f9d8a5d0f8525c",
+    "url": "/portfolio/static/media/space1.7ac0812b.png"
+  },
+  {
+    "revision": "e21eec760f48e28283d52040dfd2ca1f",
+    "url": "/portfolio/static/media/space2.e21eec76.png"
+  }
+]);
